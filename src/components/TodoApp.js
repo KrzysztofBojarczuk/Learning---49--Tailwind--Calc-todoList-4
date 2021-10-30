434310dfa9eec2691ca4cf523a6a549f50b7cab0
Krzysztofowi bojarczuk <krzysztofbojarczuk@wp.pl>
2021-10-30 10:07:12 +0200

    nauka

---
+++ src/components/TodoApp.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const TodoApp = () => {
+
+    return (
+        <React.Fragment>
+            <div className="bg-indigo-50 h-96 mt-10 rounded-lg mx-10">
+                Hello
+            </div>
+
+
+
+        </React.Fragment>
+    )
+}
+
+export default TodoApp
